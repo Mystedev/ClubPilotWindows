07a96276f4b952b569656bce916693aeda0f5144
CescAP <c.albinya@insjoanbrudieu.cat>
2025-02-24 19:29:41 +0100

    Pantalla de les comptes incorporada

---
+++ ClubPilot/Accounts.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ClubPilot
+{
+    partial class Accounts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_mod_conta = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_add_conta = new System.Windows.Forms.Button();
+            this.btn_eli_conta = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestionar comptes";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 64.90626F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.54687F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.54688F));
+            this.tableLayoutPanel1.Controls.Add(this.btn_eli_conta, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_mod_conta, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(120, 183);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 11;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.090908F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 440);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // btn_mod_conta
+            // 
+            this.btn_mod_conta.Location = new System.Drawing.Point(576, 3);
+            this.btn_mod_conta.Name = "btn_mod_conta";
+            this.btn_mod_conta.Size = new System.Drawing.Size(70, 31);
+            this.btn_mod_conta.TabIndex = 1;
+            this.btn_mod_conta.Text = "btn";
+            this.btn_mod_conta.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(364, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nom";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(701, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Modificar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(858, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Esborrar";
+            // 
+            // btn_add_conta
+            // 
+            this.btn_add_conta.Image = global::ClubPilot.Properties.Resources.icons8_añadir_30;
+            this.btn_add_conta.Location = new System.Drawing.Point(1045, 573);
+            this.btn_add_conta.Name = "btn_add_conta";
+            this.btn_add_conta.Size = new System.Drawing.Size(39, 41);
+            this.btn_add_conta.TabIndex = 3;
+            this.btn_add_conta.UseVisualStyleBackColor = true;
+            // 
+            // btn_eli_conta
+            // 
+            this.btn_eli_conta.Image = global::ClubPilot.Properties.Resources.icons8_eliminar_30;
+            this.btn_eli_conta.Location = new System.Drawing.Point(731, 3);
+            this.btn_eli_conta.Name = "btn_eli_conta";
+            this.btn_eli_conta.Size = new System.Drawing.Size(39, 33);
+            this.btn_eli_conta.TabIndex = 4;
+            this.btn_eli_conta.UseVisualStyleBackColor = true;
+            // 
+            // Accounts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 654);
+            this.Controls.Add(this.btn_add_conta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Accounts";
+            this.Text = "Accounts";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn_mod_conta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_add_conta;
+        private System.Windows.Forms.Button btn_eli_conta;
+    }
+}
