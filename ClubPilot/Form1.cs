9bc83dff499e299b13a43457e3077133f48b2f0a
Baikal <b.gomez@insjoanbrudieu.cat>
2025-02-24 16:45:50 +0100

    login añadido

--- ClubPilot/Form1.cs
+++ ClubPilot/Form1.cs
@@ -16,5 +16,50 @@ namespace ClubPilot
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if((textBox1.Text.Equals("usuario1"))&&(textBox2.Text.Equals("contraseña1")))
+            { 
+                label1.Visible = false;
+                label2.Visible = false;
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                button1.Visible = false;
+                comboBox1.Visible = true;
+                label3.Visible= true;
+                button2.Visible= true;
+                comboBox1.Items.Add("equip1");
+                comboBox1.Items.Add("equip2");
+                comboBox1.Items.Add("equip3");
+                comboBox1.Items.Add("equip4");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
