73c5f53a5838083bd932fce3b546f81af6c075e5
Baikal <b.gomez@insjoanbrudieu.cat>
2025-03-10 16:08:46 +0100

    implementados los elementos visuales de la vista para aprovar o rechazar clubs

---
+++ ClubPilot/clubs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClubPilot
+{
+    public partial class clubs : Form
+    {
+        public clubs()
+        {
+            InitializeComponent();
+        }
+
+        private void clubs_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
