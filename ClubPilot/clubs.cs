068a88552876916e377642f101a2d7521d2b3dfb
Baikal <b.gomez@insjoanbrudieu.cat>
2025-03-10 16:21:34 +0100

    Corrigiendo .gitignore

--- ClubPilot/clubs.cs
+++ ClubPilot/clubs.cs
@@ -21,5 +21,20 @@ namespace ClubPilot
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
