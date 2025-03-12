8a6d441de4295cb8e8bd9bb520683c629c15d85b
CescAP <c.albinya@insjoanbrudieu.cat>
2025-03-12 20:57:31 +0100

    Staging all changes

--- ClubPilot/Players.Designer.cs
+++ ClubPilot/Players.Designer.cs
@@ -36,6 +36,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.clubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.equipsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sollicitudsDeClubsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,49 +47,54 @@
             this.menuStrip1.BackColor = System.Drawing.Color.MidnightBlue;
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.jugadorsToolStripMenuItem,
-            this.esdevenimentsToolStripMenuItem,
+            this.sollicitudsDeClubsToolStripMenuItem,
+            this.clubToolStripMenuItem,
             this.comptesToolStripMenuItem,
-            this.noticiesToolStripMenuItem});
+            this.noticiesToolStripMenuItem,
+            this.jugadorsToolStripMenuItem,
+            this.esdevenimentsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1399, 49);
+            this.menuStrip1.Size = new System.Drawing.Size(1356, 48);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // jugadorsToolStripMenuItem
             // 
-            this.jugadorsToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jugadorsToolStripMenuItem.Font = new System.Drawing.Font("Cascadia Code", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.jugadorsToolStripMenuItem.ForeColor = System.Drawing.Color.White;
             this.jugadorsToolStripMenuItem.Name = "jugadorsToolStripMenuItem";
-            this.jugadorsToolStripMenuItem.Size = new System.Drawing.Size(156, 45);
+            this.jugadorsToolStripMenuItem.Size = new System.Drawing.Size(175, 44);
             this.jugadorsToolStripMenuItem.Text = "Jugadors";
             this.jugadorsToolStripMenuItem.Click += new System.EventHandler(this.jugadorsToolStripMenuItem_Click);
             // 
             // esdevenimentsToolStripMenuItem
             // 
-            this.esdevenimentsToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.esdevenimentsToolStripMenuItem.Font = new System.Drawing.Font("Cascadia Code", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.esdevenimentsToolStripMenuItem.ForeColor = System.Drawing.Color.White;
             this.esdevenimentsToolStripMenuItem.Name = "esdevenimentsToolStripMenuItem";
-            this.esdevenimentsToolStripMenuItem.Size = new System.Drawing.Size(237, 45);
+            this.esdevenimentsToolStripMenuItem.Size = new System.Drawing.Size(265, 44);
             this.esdevenimentsToolStripMenuItem.Text = "Esdeveniments";
+            this.esdevenimentsToolStripMenuItem.Click += new System.EventHandler(this.esdevenimentsToolStripMenuItem_Click);
             // 
             // comptesToolStripMenuItem
             // 
-            this.comptesToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comptesToolStripMenuItem.Font = new System.Drawing.Font("Cascadia Code", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comptesToolStripMenuItem.ForeColor = System.Drawing.Color.White;
             this.comptesToolStripMenuItem.Name = "comptesToolStripMenuItem";
-            this.comptesToolStripMenuItem.Size = new System.Drawing.Size(154, 45);
+            this.comptesToolStripMenuItem.Size = new System.Drawing.Size(157, 44);
             this.comptesToolStripMenuItem.Text = "Comptes";
+            this.comptesToolStripMenuItem.Click += new System.EventHandler(this.comptesToolStripMenuItem_Click);
             // 
             // noticiesToolStripMenuItem
             // 
-            this.noticiesToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noticiesToolStripMenuItem.Font = new System.Drawing.Font("Cascadia Code", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.noticiesToolStripMenuItem.ForeColor = System.Drawing.Color.White;
             this.noticiesToolStripMenuItem.Name = "noticiesToolStripMenuItem";
-            this.noticiesToolStripMenuItem.Size = new System.Drawing.Size(143, 45);
+            this.noticiesToolStripMenuItem.Size = new System.Drawing.Size(175, 44);
             this.noticiesToolStripMenuItem.Text = "Noticies";
+            this.noticiesToolStripMenuItem.Click += new System.EventHandler(this.noticiesToolStripMenuItem_Click);
             // 
             // button1
             // 
@@ -97,11 +105,7 @@
             this.button1.Location = new System.Drawing.Point(16, 64);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-
             this.button1.Size = new System.Drawing.Size(169, 41);
-
-
-
             this.button1.TabIndex = 1;
             this.button1.Text = "Futbol";
             this.button1.UseVisualStyleBackColor = true;
@@ -111,12 +115,10 @@
             // 
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-
             this.button2.Location = new System.Drawing.Point(213, 64);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(169, 41);
-
             this.button2.TabIndex = 2;
             this.button2.Text = "Basketball";
             this.button2.UseVisualStyleBackColor = true;
@@ -131,30 +133,51 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 153F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 156F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 154F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 154F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 155F));
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 108);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1025, 471);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1256, 471);
             this.tableLayoutPanel1.TabIndex = 3;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // clubToolStripMenuItem
+            // 
+            this.clubToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.equipsToolStripMenuItem});
+            this.clubToolStripMenuItem.Font = new System.Drawing.Font("Cascadia Code", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clubToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.clubToolStripMenuItem.Name = "clubToolStripMenuItem";
+            this.clubToolStripMenuItem.Size = new System.Drawing.Size(103, 44);
+            this.clubToolStripMenuItem.Text = "Club";
+            this.clubToolStripMenuItem.Click += new System.EventHandler(this.clubToolStripMenuItem_Click);
+            // 
+            // equipsToolStripMenuItem
+            // 
+            this.equipsToolStripMenuItem.BackColor = System.Drawing.Color.MidnightBlue;
+            this.equipsToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.equipsToolStripMenuItem.Name = "equipsToolStripMenuItem";
+            this.equipsToolStripMenuItem.Size = new System.Drawing.Size(224, 44);
+            this.equipsToolStripMenuItem.Text = "Equips";
+            // 
+            // sollicitudsDeClubsToolStripMenuItem
+            // 
+            this.sollicitudsDeClubsToolStripMenuItem.Font = new System.Drawing.Font("Cascadia Code", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sollicitudsDeClubsToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.sollicitudsDeClubsToolStripMenuItem.Name = "sollicitudsDeClubsToolStripMenuItem";
+            this.sollicitudsDeClubsToolStripMenuItem.Size = new System.Drawing.Size(409, 44);
+            this.sollicitudsDeClubsToolStripMenuItem.Text = "Sol·licituds de clubs";
+            this.sollicitudsDeClubsToolStripMenuItem.Click += new System.EventHandler(this.sollicitudsDeClubsToolStripMenuItem_Click);
             // 
             // Players
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaShell;
-
-            this.ClientSize = new System.Drawing.Size(1399, 727);
-
-
-
-            this.ClientSize = new System.Drawing.Size(1049, 591);
+            this.ClientSize = new System.Drawing.Size(1356, 591);
             this.Controls.Add(this.tableLayoutPanel1);
-
-
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
@@ -180,5 +203,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStripMenuItem clubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem equipsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sollicitudsDeClubsToolStripMenuItem;
     }
-}
+    }
