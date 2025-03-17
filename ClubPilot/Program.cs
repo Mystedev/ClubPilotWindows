a2711caf58dc68ff266b7f9a6f292b0f9485b227
Dennys <moya-deny14@hotmail.com>
2025-03-17 17:02:27 +0100

    Punt

--- ClubPilot/Program.cs
+++ ClubPilot/Program.cs
@@ -16,7 +16,7 @@ namespace ClubPilot
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Accounts());
+            Application.Run(new Esdeveniments());
         }
     }
 }
